5b8c10ae00c423f97a4f6355be30c9d1ee93a808
Juan Rivera <juanriv45@gmail.com>
2018-02-10 21:52:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(word)
   list_reversed
   list_of_words = word.split(" ")
   list_of_words.each {|x| list_reversed = x.reverse,
-     list_reversed.join(" ")}
+  list_reversed.join(" ")}
 
 end
