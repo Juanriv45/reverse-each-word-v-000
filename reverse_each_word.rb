88e326c3914a597225f5c46f581810700bf90cc9
Juan Rivera <juanriv45@gmail.com>
2018-02-10 21:53:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(word)
   list_reversed = []
   list_of_words = word.split(" ")
-  list_of_words.each {|x| list_reversed = x.reverse,
+  list_of_words.collect {|x| list_reversed = x.reverse,
   list_reversed.join(" ")}
 
 end
